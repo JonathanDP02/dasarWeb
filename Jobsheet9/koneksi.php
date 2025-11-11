df2964c06ec766cc7171fb353614440af1386486
JonathanDP02 <jonatanpangestu23@gmail.com>
2025-11-12 03:17:23 +0700

    aduhaiii

--- Jobsheet9/koneksi.php
+++ Jobsheet9/koneksi.php
@@ -1,26 +1,30 @@
 <?php
-$koneksi = mysqli_connect("localhost", "root", "", "prakwebdb");
+$connect = mysqli_connect("localhost", "root", "", "prakwebdb");
 
-if (!$koneksi) {
+if (!$connect) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-$sql = "CREATE TABLE user (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL
-)";
+// $sql = "CREATE TABLE user (
+//     id INT(11) AUTO_INCREMENT PRIMARY KEY,
+//     username VARCHAR(50) NOT NULL,
+//     password VARCHAR(100) NOT NULL
+// )";
 
-$username = "admin";
-$password = md5("12345");
+// $username = "admin";
+// $password = ("123");
+// $level= ("1");
 
-$sql = "INSERT INTO user (username, password) VALUES ('$username', '$password')";
+// $username = "guest";
+// $password = ("234");
+// $level = ("2");
 
-if (mysqli_query($koneksi, $sql)) {
-    echo "Tabel user berhasil dibuat";
-} else {
-    echo "Error membuat tabel: " . mysqli_error($koneksi);
-}
+// $sql = "INSERT INTO user (username, password) VALUES ('$username', '$password')";
 
-mysqli_close($koneksi);
+// if (mysqli_query($connect, $sql)) {
+//     echo "Tabel user berhasil dibuat";
+// } else {
+//     echo "Error membuat tabel: " . mysqli_error($connect);
+// }
+
 ?>
